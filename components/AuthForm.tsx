cf3a16e865019059d7332f542a530129861a6f61
Dwaynew85 <mr_dwayne_w@yahoo.com>
2024-09-19 16:09:07 -0400

    full sign in functioning

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -50,13 +50,13 @@ const AuthForm = ({ type }: {type: string}) => {
         setUser(newUser)
       }
 
-      // if(type === 'sign-in') {
-      //   const response = await signIn({
-      //     email: data.email,
-      //     password: data.password
-      //   })
-      //   if(response) router.push('/')
-      // }
+      if(type === 'sign-in') {
+        const response = await signIn({
+          email: data.email,
+          password: data.password
+        })
+        if(response) router.push('/')
+      }
       console.log(data)
       setisLoading(false)
     } catch (error) {
